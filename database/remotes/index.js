a12c69982c4d2986527c9e757ca70d232f374b30
nonthasak laoluerat <nonthasak.l@Appels-iMac.local>
2022-11-04 07:07:37 +0700

    add remote database

--- database/remotes/index.js
+++ database/remotes/index.js
@@ -20,6 +20,7 @@ async function remoteAddVerifiedHolder({
   );
 
   console.log(response.data);
+  return response.data;
 }
 
 async function remoteUpdateVerifiedHolder({ nftAddress, discordId, verified }) {
@@ -34,9 +35,26 @@ async function remoteUpdateVerifiedHolder({ nftAddress, discordId, verified }) {
     }
   );
   console.log(response.data);
+  return response.data;
+}
+
+async function remoteGetVerifiedHolders(nftAddress) {
+  const response = await getHolders(nftAddress);
+  const verified = response.filter((data) => data.verified === false);
+  return verified;
+}
+
+async function remoteGetHolders(nftAddress) {
+  const response = await axios.get(
+    `${process.env.remote_database}/${nftAddress}`
+  );
+
+  return response.data;
 }
 
 module.exports = {
   remoteAddVerifiedHolder,
   remoteUpdateVerifiedHolder,
+  remoteGetHolders,
+  remoteGetVerifiedHolders,
 };
