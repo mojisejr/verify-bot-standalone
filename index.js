459fd9523831c93a4ac459c60c8e9c335fda4993
nonthask laoluerat <nonthasak@Macintosh.local>
2022-11-05 14:40:30 +0700

    updates

--- index.js
+++ index.js
@@ -67,5 +67,6 @@ sequelize
   .sync()
   .then(() => console.log(chalk.bgGreenBright("Database: ONLINE")));
 server.listen(PORT, () => {
+  console.log(__dirname);
   console.log(chalk.bgGreenBright("API: ONLINE on port - ", PORT));
 });
