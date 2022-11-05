cc39c7cf1a13f7192b5df6fce84852f6777e844c
nonthask laoluerat <nonthasak@Macintosh.local>
2022-11-05 14:06:12 +0700

    update config for database

--- database/sqlite/sqlite.database.js
+++ database/sqlite/sqlite.database.js
@@ -4,6 +4,7 @@ const basePath = process.cwd();
 const sequelize = new Sequelize("game-state-db", "user", "pass", {
   dialect: "sqlite",
   host: `${basePath}/database/sqlite/db.sqlite`,
+  logging: false,
 });
 
 module.exports = sequelize;
